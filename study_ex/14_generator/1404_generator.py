22003ea3cdd8a855d982a66c4d98be5e652e7ff0
kadragon <kadragon@sasa.hs.kr>
2018-09-26 21:56:20 +0900

    * 13_decorator_name_fix, 1404_generator

--- study_ex/14_generator/1404_generator.py
+++ study_ex/14_generator/1404_generator.py
@@ -4,14 +4,25 @@ size = 100000
 
 # List 형식
 a = [i for i in range(size)]
+
 print(a)
 # 결과: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...
+
 print("Memory: %d" % sys.getsizeof(a))
 # 결과: Memory: 824464
 
+print(len(list(a)))  # 결과: 100000
+print(len(list(a)))  # 결과: 100000
+
+
 # Generator 형식
 b = (i for i in range(size))
+
 print(b)
 # 결과: <generator object <genexpr> at 0x107996048>
+
 print("Memory: %d" % sys.getsizeof(b))
 # 결과: Memory: 120
+
+print(len(list(b)))  # 결과: 100000
+print(len(list(b)))  # 결과: 0
